c2067e9091c7c55b3223b418ee8ce9ba74dab914
Luis <m.luishernandez@outlook.com>
2020-08-28 12:59:49 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World From C#");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Esto Es Una Prueba");
+            
+        
+
+            //This is a comment
+            
+            /* The comment below ill be multi line and it will end
+            below this full string of words */
+
+        }
+    }
+}
+
+
+
+
